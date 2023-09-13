0952ed230177cdbbce7394478622b784fc6cb8e0
Lauda D Raka <laudadraka@gmail.com>
2023-09-13 23:43:38 +0700

    Moved the Inventory

--- World/Player/player.gd
+++ World/Player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var stats : Player_Statistic
 @export var Bullet : PackedScene
 @export var weapon_used : Weapon_Statistic
+@export var inventory : Player_Inventory
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
