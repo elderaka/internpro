ff82390e533911b448d1475c66a638298e4a96ce
Macrune <pesonaponasa@gmail.com>
2023-09-17 00:15:33 +0700

    Shop and story UI
    Added UI for shop and story but havent been connected to change scene and world

--- World/test_world.gd
+++ World/test_world.gd
@@ -3,6 +3,20 @@ extends Node2D
 
 @onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
 @onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+@onready var reward = $Reward
 
 func _ready():
 	polygon_2d.polygon = collision_polygon_2d.polygon
+
+func _physics_process(delta):
+	if get_tree().get_nodes_in_group("enemy").is_empty():
+		levelFinished()
+
+func levelFinished():
+	if not get_tree().paused:
+		pause()
+	reward.visible = true
+
+func pause():
+	var newPausedState = not get_tree().paused
+	get_tree().paused = newPausedState
